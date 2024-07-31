ff653a0642ff64ab9046ab22d8682e98d1a5166d
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-08-01 00:20:38 +0600

    ADD : Project initialization

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c7d2952abad008606347');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '38150c16d043e1524d76');
